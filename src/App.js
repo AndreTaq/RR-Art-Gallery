8322c2f406cd0f0c7f8d487f85984f87a9d41811
AndreTaq <andretaqian@gmail.com>
2024-03-23 21:19:06 -0700

    added middleware

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+// import { useState, useEffect } from 'react';
+import { useSelector, useDispatch, connect} from 'react-redux'
+import { fetchData, clearData, inputId, incrementTd, decrementTd } from './features/dataSlice'
+import { useEffect } from 'react';
 
-function App() {
+
+function App(props) {
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.data)
+
+  const renderImg = () => {
+    if (data.apiData) {
+      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p> No image </p>
+    }
+  }
+
+  useEffect(() => {
+    dispatch(fetchData())
+  }, [props.objectId, dispatch])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Welcome to our Gallery site!!</h1>
+      <button onClick={() => dispatch(fetchData())}>Thunk</button>
+      <button onClick={() => dispatch(clearData())}>Clear</button>
+      <button onClick={() => dispatch(incrementTd())}>Next</button>
+      <button onClick={() => dispatch(decrementTd())}>Back</button>
+      <div>
+        <input value={ data.objectId } onChange={(e) => {dispatch(inputId(Number(e.target.value)))}} />
+        <div>
+          {data.objectId}
+          {renderImg()}
+        </div>
+      </div>
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+
+const mapStateToProps = (state, ownProps) => ({
+  objectId: state.data.objectId
+});
+
+export default connect(mapStateToProps)(App)
